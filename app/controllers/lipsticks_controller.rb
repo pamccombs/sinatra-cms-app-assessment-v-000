9d0af0a3db4cbf1416975a4d5eb56b0d7cc4953e
Patrick McCombs <pa.mccombs1@gmail.com>
2019-05-03 21:02:48 +0000

    shows username in lipsticks.erb

--- app/controllers/lipsticks_controller.rb
+++ app/controllers/lipsticks_controller.rb
@@ -2,12 +2,9 @@ class LipsticksController <ApplicationController
 
   get '/lipsticks' do
     if logged_in?
-      #@lipsticks= Lipstick.all
       @users = User.all
       #@users each do |user|
         #user.lipsticks each |lipstick| do
-        
-          
       erb :'lipsticks/lipsticks'
     else
       redirect to '/login'
