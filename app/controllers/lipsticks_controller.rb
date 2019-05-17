6b5300977cc91891263b151005adba50a8541bcf
Patrick McCombs <pa.mccombs1@gmail.com>
2019-05-17 22:05:42 +0000

    fix code format of lipsticks controller

--- app/controllers/lipsticks_controller.rb
+++ app/controllers/lipsticks_controller.rb
@@ -9,7 +9,6 @@ class LipsticksController <ApplicationController
     end
   end
 
-
   get '/lipsticks/new' do
     if logged_in?
       erb :'lipsticks/new'
@@ -18,12 +17,11 @@ class LipsticksController <ApplicationController
     end
   end
 
-
   post '/lipsticks' do
     if logged_in?
       if params[:color_scheme_main] == ""
         redirect '/lipsticks/new'
-    else
+      else
       @lipstick = Lipstick.create(
         color_scheme_main: params[:color_scheme_main], 
         color_scheme_undertone: params[:color_scheme_undertone], 
@@ -46,18 +44,18 @@ class LipsticksController <ApplicationController
     end
   end
 
-
   get '/lipsticks/:id' do
     if logged_in?
       @lipstick = Lipstick.find_by_id(params[:id])
-        erb :'lipsticks/show_lipstick'
+      erb :'lipsticks/show_lipstick'
     else
-        redirect '/login'
-      end
+      redirect '/login'
+    end
   end
 
   get '/lipsticks/:id/edit' do
-   @lipstick = Lipstick.find_by_id(params[:id])
+    @lipstick = Lipstick.find_by_id(params[:id])
+    
     if logged_in? && @lipstick.user_id == session[:user_id]
       erb :'lipsticks/edit_lipstick'
     else
@@ -69,42 +67,43 @@ class LipsticksController <ApplicationController
     if logged_in?
       if params[:color_scheme_main] == ""
           redirect "/lipsticks/#{params[:id]}/edit"
-    else
+      else
         @lipstick = Lipstick.find_by_id(params[:id])
-    if @lipstick && @lipstick.user == current_user
-        if @lipstick.update(
-          color_scheme_main: params[:color_scheme_main],
-          color_scheme_undertone: params[:color_scheme_undertone],
-          name_of_lipstick: params[:name_of_lipstick],
-          finish: params[:finish],
-          dryness: params[:dryness],
-          requires_a_base: params[:requires_a_base],
-          longevity: params[:longevity],
-          brand: params[:brand],
-          difficulty_to_apply: params[:difficulty_to_apply],
-          difficulty_to_remove: params[:difficulty_to_remove],
-          oil_or_water_removal: params[:oil_or_water_removal]
-          )
-          redirect "/lipsticks/#{@lipstick.id}"
-    else
+        if @lipstick && @lipstick.user == current_user
+          if @lipstick.update(
+            color_scheme_main: params[:color_scheme_main],
+            color_scheme_undertone: params[:color_scheme_undertone],
+            name_of_lipstick: params[:name_of_lipstick],
+            finish: params[:finish],
+            dryness: params[:dryness],
+            requires_a_base: params[:requires_a_base],
+            longevity: params[:longevity],
+            brand: params[:brand],
+            difficulty_to_apply: params[:difficulty_to_apply],
+            difficulty_to_remove: params[:difficulty_to_remove],
+            oil_or_water_removal: params[:oil_or_water_removal]
+            )
+            redirect "/lipsticks/#{@lipstick.id}"
+          else
           redirect "/lipsticks/#{@lipstick.id}/edit"
           end
-    else
+        else
           redirect '/lipsticks'
         end
-    end
-    else
-          redirect '/login'
-        end
-    end
-    
-    delete '/lipsticks/:id/delete' do
-        @lipstick = Lipstick.find_by_id(params[:id])
-      if logged_in? && @lipstick.user_id == session[:user_id]
-        @lipstick.delete
-        redirect '/lipsticks'
-      else
-        redirect '/login'
       end
+    else
+      redirect '/login'
     end
+  end
+    
+  delete '/lipsticks/:id/delete' do
+    @lipstick = Lipstick.find_by_id(params[:id])
+    
+    if logged_in? && @lipstick.user_id == session[:user_id]
+      @lipstick.delete
+      redirect '/lipsticks'
+    else
+      redirect '/login'
+    end
+  end
 end
