2e79862917e28e0345c470200860cba7c5039ee9
pamccombs <pa.mccombs1@gmail.com>
2019-05-20 17:07:50 -0400

    create a new helper method and refactor

--- app/controllers/lipsticks_controller.rb
+++ app/controllers/lipsticks_controller.rb
@@ -1,61 +1,49 @@
 class LipsticksController < ApplicationController
 
   get '/lipsticks' do
-    if logged_in?
-      @users = User.all
-      erb :'lipsticks/lipsticks'
-    else
-      redirect to '/login'
-    end
+    redirect_if_not_logged_in
+    @users = User.all
+    erb :'lipsticks/lipsticks'
   end
 
   get '/lipsticks/new' do
-    if logged_in?
-      erb :'lipsticks/new'
-    else
-      redirect '/login'
-    end
+    redirect_if_not_logged_in
+    erb :'lipsticks/new'
   end
 
   post '/lipsticks' do
-    if logged_in?
-      if params[:color_scheme_main] == ""
-        redirect '/lipsticks/new'
-      else
-      @lipstick = Lipstick.create(
-        color_scheme_main: params[:color_scheme_main], 
-        color_scheme_undertone: params[:color_scheme_undertone], 
-        name_of_lipstick: params[:name_of_lipstick], 
-        finish: params[:finish], 
-        dryness: params[:dryness], 
-        requires_a_base: params[:requires_a_base], 
-        longevity: params[:longevity], 
-        brand: params[:brand], 
-        difficulty_to_apply: params[:difficulty_to_apply], 
-        difficulty_to_remove: params[:difficulty_to_remove], 
-        oil_or_water_removal: params[:oil_or_water_removal]
-        )
-      @lipstick.user_id = current_user.id
-      @lipstick.save
-        redirect "/lipsticks/#{@lipstick.id}"
-      end
+    redirect_if_not_logged_in
+    if params[:color_scheme_main] == ""
+      redirect '/lipsticks/new'
     else
-      redirect '/login'
+    #@lipstick = Lipstick.create(
+    #  color_scheme_main: params[:color_scheme_main],
+    #  color_scheme_undertone: params[:color_scheme_undertone],
+    #  name_of_lipstick: params[:name_of_lipstick],
+    #  finish: params[:finish],
+    #  dryness: params[:dryness],
+    #  requires_a_base: params[:requires_a_base],
+    #  longevity: params[:longevity],
+    #  brand: params[:brand],
+    #  difficulty_to_apply: params[:difficulty_to_apply],
+    #  difficulty_to_remove: params[:difficulty_to_remove],
+    #  oil_or_water_removal: params[:oil_or_water_removal]
+    #  )
+    @lipstick = current_user.lipsitcks.build(params)
+    @lipstick.save
+    redirect "/lipsticks/#{@lipstick.id}"
     end
   end
 
   get '/lipsticks/:id' do
-    if logged_in?
-      @lipstick = Lipstick.find_by_id(params[:id])
-      erb :'lipsticks/show_lipstick'
-    else
-      redirect '/login'
-    end
+    redirect_if_not_logged_in
+    @lipstick = Lipstick.find_by_id(params[:id])
+    erb :'lipsticks/show_lipstick'
   end
 
   get '/lipsticks/:id/edit' do
     @lipstick = Lipstick.find_by_id(params[:id])
-    
+
     if logged_in? && @lipstick.user_id == session[:user_id]
       erb :'lipsticks/edit_lipstick'
     else
@@ -95,10 +83,10 @@ class LipsticksController < ApplicationController
       redirect '/login'
     end
   end
-    
+
   delete '/lipsticks/:id/delete' do
     @lipstick = Lipstick.find_by_id(params[:id])
-    
+
     if logged_in? && @lipstick.user_id == session[:user_id]
       @lipstick.delete
       redirect '/lipsticks'
@@ -106,5 +94,5 @@ class LipsticksController < ApplicationController
       redirect '/login'
     end
   end
-  
+
 end
